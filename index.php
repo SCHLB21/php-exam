9f6aef46ca55d547e5866b4221a47edb8c4dc9c5
Dmitriy <dmitryweazy@gmail.com>
2020-06-25 13:27:56 +0300

    24

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require "includes/input_type.php";
         $get_link ="SELECT * FROM `sessions` WHERE `session_link`= '".$_GET['link']."'";
         $result = mysqli_query($link, $get_link) or die("Ошибка " . mysqli_error($link));
         if(mysqli_num_rows($result)!=0){
-            echo '<h1>Опрос на тему'.mysqli_fetch_row($result)[2].'</h1>
+            echo '<h1>Опрос на тему "'.mysqli_fetch_row($result)[2].'"</h1>
             <form action="/" method="post">';
             $get_session = "SELECT * FROM `".$_GET['link']."`";
             $session = mysqli_query($link, $get_session) or die("Ошибка " . mysqli_error($link));
@@ -26,6 +26,7 @@ require "includes/input_type.php";
                 if($for%2==0){continue;}
                 echo '<label for="'.$key.'">'.$value.'</label><input '.get_type($s_row[$key.'_type']).' required></br>';
             }
+            echo '<input type="submit" value="Отправить">';
         }else{
             echo '<h1>Некорректная ссылка</h1>';
         }
