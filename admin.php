957bbfe2ccd60d909d0fc52524655a25a1c658fb
Dmitriy <dmitryweazy@gmail.com>
2020-06-25 15:41:05 +0300

    24

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@
 <body>
 <?php
 if(!empty($_POST)|| $_GET['status']='add'){
-    if($_POST['password']=='12345'){
+    if($_POST['password']=='12345' || $_GET['status']='add'){
         echo 'Доступ получен </br>';
         echo '<a href="?status=add">Добавить новую сессию</a>';
         if($_GET['status']=='add'){
