63d4a75f91b6e9d987c59f6f183272fcc96f984a
Dmitriy <dmitryweazy@gmail.com>
2020-06-25 14:42:16 +0300

    24

--- index.php
+++ index.php
@@ -60,11 +60,12 @@ require "includes/input_type.php";
         }
         $elements = mb_substr($elements, 0, -1);
         $elements = mb_substr($elements, 0, -1);
-        $elements.=" '";
+        $elements.="'";
         echo $elements;
 //        print_r($_POST);
 
-//        $post_query ="INSERT INTO ".$table_name."VALUES('".$;
+        $post_query ="INSERT INTO ".$table_name."VALUES(".$elements.")";
+        $add_answer = mysqli_query($link, $post_query) or die("Ошибка " . mysqli_error($link));
     }
     mysqli_close($link);
     ?>
