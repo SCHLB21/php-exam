6223f30f3f095def7a0c310e936c0c4807fcaefb
Dmitriy <dmitryweazy@gmail.com>
2020-06-25 11:32:59 +0300

    1/3

--- index.php
+++ index.php
@@ -1,26 +1,10 @@
 <?php require "includes/bd.php"; ?>
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Комков Дмитрий 191-322</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Опрос на тему "Вставить текст"</h1>
-        <form action="/" method="post">
-        <label for="question"></label><input type="text" id="question">
-        </form>
-    </div>
     <?php
     $query ="SELECT * FROM `sessions`";
     $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
     while($row = mysqli_fetch_row($result)){
 
-        echo $row[0];
+        echo $row[0], $row[1];
     }
     mysqli_close($link);
     ?>
-</body>
-</html>
