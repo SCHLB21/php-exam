8ab92d60bb1f8d3e7443b3948a5186548ca8ca0e
Dmitriy <dmitryweazy@gmail.com>
2021-01-28 11:45:21 +0300

    111

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function radio(id){
     let element = document.getElementById('theme'+id);
     console.log(element.value)
+    if(element.value=='checkbox'){
+        element.insertAdjacentHTML("afterEnd", "<label for='checkbox"+id+"'> Количество вариантов ответа: </label></label><input type='number' id='checkbox"+id+"'>");
+    }
     if(element.value=='radio'){
         element.insertAdjacentHTML("afterEnd", "<label for='radio"+id+"'> Количество вариантов ответа: </label></label><input type='number' id='radio"+id+"'>");
     }
