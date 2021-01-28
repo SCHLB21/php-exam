9cb34383b8194398bfc695567ff682a5e01147af
Dmitriy <dmitryweazy@gmail.com>
2021-01-28 11:43:30 +0300

    111

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function radio(id){
     let element = document.getElementById('theme'+id);
     console.log(element.value)
     if(element.value=='radio'){
-        element.insertAdjacentHTML("afterEnd", "<input type='number'>");
+        element.insertAdjacentHTML("afterEnd", "<label for='radio"+id+"'> Количество вариантов ответа: </label></label><input type='number' id='radio"+id+"'>");
     }
 }
