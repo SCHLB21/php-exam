809706ae52e0403e4155b96e1d124277d1f4d2fc
Dmitriy <dmitryweazy@gmail.com>
2021-01-28 11:29:03 +0300

    111

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function radio(id){
     let element = document.getElementById('theme'+id);
     console.log(element)
     if(element.value=='radio'){
-        // element.innerHTML(eleme)
+        element.innerHTML(element+'<input type="number">')
     }
 }
