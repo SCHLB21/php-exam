f4d9a482b836cb5da7edb9c27a6d9ad6578aaf6f
Dmitriy <dmitryweazy@gmail.com>
2021-01-28 12:59:21 +0300

    111

--- admin.php
+++ admin.php
@@ -79,8 +79,10 @@ if(!empty($_POST)|| $_GET['status']){
                     }
                     $questions[$i]['answer']=$_POST['answer'.$i];
                 }
-                print_r($questions);
-                echo json_encode($questions);
+                $questions = json_encode($questions);
+                $theme = $_POST['theme'];
+                $questions_query="INSERT INTO `std_924.sessions` (session_link, session_status, theme, questions) 
+                        VALUES ($session_link, 'active', $theme, $questions)";
 //                $newSession="CREATE TABLE `std_924.".".".$session_link."`";
             }
             echo '</form>';
